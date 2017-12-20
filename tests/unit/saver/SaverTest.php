1a130425057e497ff3faf8deac3c085b213aeb66
konstantin <t.kanstantsin@gmail.com>
2017-12-21 02:33:14 +0300

    test - create test road map for file saver

---
+++ tests/unit/saver/SaverTest.php
@@ -0,0 +1,75 @@
+<?php
+
+
+class SaverTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function testWhetherSaved()
+    {
+        // TODO: create component which would generate FileManager and it's components.
+        // TODO: use memory adapter and create each time completely new FileManager.
+        /**
+         * Steps:
+         * - get FS
+         * - get IFile
+         * - get asset path
+         *
+         * - check if NOT saved
+         *
+         * --//--
+         * - write something random in asset path in FS
+         * - check if IS saved
+         *
+         * --//--
+         * - write something random in asset path in FS
+         * - set updatedAt into future
+         * - check if NOT saved
+         */
+    }
+
+    public function testWrite()
+    {
+        /**
+         * Steps:
+         * - get saver
+         * - try write null or false -> get false
+         *
+         * - get saver
+         * - try write anything else -> get true or exception
+         */
+    }
+
+    public function testSave()
+    {
+        /**
+         * Steps:
+         * - get manager, fs, file
+         * - save file into path -> return true and file created in fs
+         */
+    }
+
+    public function testSaveSkipIfFileExist()
+    {
+        /**
+         * Steps:
+         * - get manager, fs, file
+         * - write file into path
+         * - set updateAt = time() - 10
+         * - save file -> return true, but file not changed
+         */
+    }
+
+    public function testForceSave()
+    {
+        /**
+         * Steps:
+         * - get manager, fs, file
+         * - add empty file into path
+         * - save with force flag -> file must be changed
+         */
+    }
+}
