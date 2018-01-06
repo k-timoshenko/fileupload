cd945c65dd167271dd05ac55d50e21b95365feea
konstantin <t.kanstantsin@gmail.com>
2018-01-07 01:21:30 +0300

    formatter - create imagine driver

---
+++ src/file/formatter/ImagineFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace tkanstantsin\fileupload\formatter;
+
+use Imagine\Gd\Imagine as GdImagine;
+use Imagine\Gmagick\Imagine as GmagickImagine;
+use Imagine\Image\ImagineInterface;
+use Imagine\Imagick\Imagine as ImagickImagine;
+use tkanstantsin\fileupload\config\InvalidConfigException;
+
+/**
+ * Class ImageDriverFactory
+ */
+class ImagineFactory
+{
+    /* Available driver */
+    public const GD = 'gd';
+    public const IMAGICK = 'imagick';
+    public const GMAGICK = 'gmagick';
+
+    /**
+     * Create imagine object for given driver
+     * @param string $type
+     * @return ImagineInterface
+     * @throws \Imagine\Exception\RuntimeException
+     * @throws InvalidConfigException
+     */
+    public static function get(string $type): ImagineInterface
+    {
+        switch ($type) {
+            case self::GD:
+                return new GdImagine();
+            case self::IMAGICK:
+                return new ImagickImagine();
+            case self::GMAGICK:
+                return new GmagickImagine();
+            default:
+                throw new InvalidConfigException(sprintf('Image driver %s not found.', $type));
+        }
+    }
+}
